955a19e40e7b89bdf69c5059518525ecf12051f9
Dylan Park <dylpark@protonmail.com>
2021-10-31 16:37:45 +1000

    Fixing typo in route

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,7 @@ use App\Http\Controller\API\PlantController;
 |
 */
 
-Route::post('/add-plants'. [PlantController::class, 'store'])
+Route::post('/add-plant', [PlantController::class, 'store']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
